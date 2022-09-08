3af4f237e62df3a94ccdfbb08bffe86a38c4efd4
Gabe Joseph <gjoseph92@gmail.com>
2022-09-08 13:39:05 -0600

    changed some test values

--- tests/test_test.py
+++ tests/test_test.py
@@ -8,9 +8,11 @@ def test_benchmark_half(auto_benchmark_time):
 
 
 def test_benchmark_quarter(auto_benchmark_time):
-    time.sleep(0.25)
+    # time.sleep(0.25)
+    time.sleep(0.1)
 
 
-@pytest.mark.parametrize("amt", [0.1, 0.3, 0.5])
+@pytest.mark.parametrize("amt", [0.3, 0.5, 0.7])
+# @pytest.mark.parametrize("amt", [0.1, 0.3, 0.5])
 def test_benchmark_param(auto_benchmark_time, amt):
     time.sleep(amt)
