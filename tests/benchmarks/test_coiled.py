9b4ffef6ec635cb6972838f9fd5bb912a6319e36
Gabe Joseph <gjoseph92@gmail.com>
2022-09-26 14:32:57 -0600

    skip coiled, parquet, work-stealing

--- tests/benchmarks/test_coiled.py
+++ tests/benchmarks/test_coiled.py
@@ -1,6 +1,9 @@
+import pytest
 from coiled import Cluster
 
 from snakebench.clusters import CLUSTER_KWARGS
+
+pytestmark = pytest.mark.skip
 
 
 def test_default_cluster_spinup_time(module_id, auto_benchmark_time):
