de8deed6de532eda0406f0c281ec1dee13ef24e8
Gabe Joseph <gjoseph92@gmail.com>
2022-10-05 23:00:48 -0600

    remove test_coiled entirely; not useful here

--- tests/benchmarks/test_coiled.py
+++
@@ -1,9 +0,0 @@
-from coiled import Cluster
-
-from snakebench.clusters import CLUSTER_KWARGS
-
-
-def test_default_cluster_spinup_time(module_id, auto_benchmark_time):
-
-    with Cluster(name=module_id, **CLUSTER_KWARGS):
-        pass
