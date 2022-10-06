bef25ec11bddb9109caddd35f12469f9f0c0062d
Gabe Joseph <gjoseph92@gmail.com>
2022-10-06 09:50:44 -0600

    fix benchmarking detection [skip ci]

--- snakebench/skip.py
+++ snakebench/skip.py
@@ -2,7 +2,8 @@ import os
 
 import pytest
 
-BENCHMARKING = os.environ.get("BENCHMARKING", "false").lower() == "true"
+# Ideally we'd use our `commit_info` fixture, but you can't easily use a fixture as input to `skipif`.
+BENCHMARKING = os.environ.get("GITHUB_REF_NAME", "").startswith("bench/")
 
 
 def skip_bench(reason: str) -> pytest.MarkDecorator:
