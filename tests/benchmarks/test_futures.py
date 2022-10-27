9ce5e798191f9f997a14a02425459e2db9673805
Gabe Joseph <gjoseph92@gmail.com>
2022-10-26 21:39:22 -0600

    fix futures tests
    they were broken because utils_test was trying to import pytest and other garbage. this is probably also why `test_memory_efficient` was failing. something broke restarting the cluster when 100k tasks were errored on it.

--- tests/benchmarks/test_futures.py
+++ tests/benchmarks/test_futures.py
@@ -1,9 +1,23 @@
+from functools import partial
+from operator import add
+from time import sleep
+from typing import Any
+
 import numpy as np
 import pytest
-from dask.distributed import as_completed, wait
-from distributed.utils_test import inc, slowdec, slowinc
+from dask.distributed import as_completed
 
 from snakebench import skip_bench
+from snakebench.utils_test import wait
+
+# NOTE: we can't import helpers like `inc`, `slowinc` from `distributed.utils_test` because
+# `utils_test` requires importing all sorts of junk like pytest, which we intentionally don't install
+# on the cluster.
+# And we can't define them top-level in this file, since then they'd be pickled by reference,
+# and this local file isn't available on the cluster.
+# Would be nice to have a better solution for this.
+
+inc = partial(add, 1)
 
 
 @pytest.mark.skip("duration too short to measure effectively")
@@ -12,11 +26,10 @@ def test_single_future(small_client):
     small_client.submit(inc, 1).result()
 
 
-@skip_bench("high memory use is nonsensical, plus too much variation to be useful")
 def test_large_map(small_client):
     """What's the overhead of map these days?"""
     futures = small_client.map(inc, range(100_000))
-    wait(futures)
+    wait(futures, small_client, 10 * 60)
 
 
 @skip_bench("too much variation to be useful")
@@ -31,13 +44,20 @@ def test_large_map_first_work(small_client):
         return
 
 
-@pytest.mark.skip("fails due to previous test")
-# FIXME the `assert not self.tasks` in `Scheduler.restart` fails after the previous test.
 def test_memory_efficient(small_client):
     """
     We hope that we pipeline xs->ys->zs without keeping all of the xs in memory
     to start.  This may not actually happen today.
     """
+
+    def slowinc(x, delay=0.02):
+        sleep(delay)
+        return x + 1
+
+    def slowdec(x, delay=0.02):
+        sleep(delay)
+        return x - 1
+
     xs = small_client.map(np.random.random, [1_000_000] * 100, pure=False)
     ys = small_client.map(slowinc, xs, delay=0.1)
     zs = small_client.map(slowdec, ys, delay=0.1)
@@ -45,5 +65,7 @@ def test_memory_efficient(small_client):
     futures = as_completed(zs)
     del xs, ys, zs  # Don't keep references to intermediate results
 
+    future: Any
     for future in futures:  # pass through all futures, forget them immediately
-        continue
+        if future.status in ("error", "cancelled"):
+            raise future.exception()
