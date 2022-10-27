bd35811c9ba74e538f389d4bf830087247aab3d9
Gabe Joseph <gjoseph92@gmail.com>
2022-10-26 22:10:41 -0600

    consolidate test_custom and test_futures
    name isn't quite accurate, but we didn't need separate clusters for these

--- tests/benchmarks/test_futures.py
+++ tests/benchmarks/test_futures.py
@@ -1,10 +1,14 @@
+import random
+import time
 from functools import partial
 from operator import add
 from time import sleep
 from typing import Any
 
 import numpy as np
+from dask import delayed
 from dask.distributed import as_completed
+from dask.utils import parse_bytes
 
 from snakebench import skip_bench
 from snakebench.utils_test import wait
@@ -69,3 +73,29 @@ def test_memory_efficient(small_client):
     for future in futures:  # pass through all futures, forget them immediately
         if future.status in ("error", "cancelled"):
             raise future.exception()
+
+
+def test_jobqueue(small_client):
+    # Just using dask to run lots of embarrassingly-parallel CPU-bound tasks as fast as possible
+    nthreads = sum(
+        w["nthreads"] for w in small_client.scheduler_info()["workers"].values()
+    )
+    max_runtime = 120
+    max_sleep = 3
+    n_tasks = round(max_runtime / max_sleep * nthreads)
+
+    @delayed(pure=True)
+    def task(i: int) -> int:
+        stuff = "x" * parse_bytes("400MiB")
+        time.sleep(random.uniform(0, max_sleep))
+        del stuff
+        return i
+
+    tasks = [task(i) for i in range(n_tasks)]
+    result = delayed(sum)(tasks)  # just so we have a single object
+
+    wait(
+        result.persist(),
+        small_client,
+        max_runtime * 1.15,
+    )
